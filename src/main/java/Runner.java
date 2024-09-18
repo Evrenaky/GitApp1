79188ca1dfaa95c43d8a152747fc91ed398594b3
Evren Akyuz <evrenaky@gmail.com>
2024-09-18 13:47:41 +0300

    yeniden c3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,7 +4,6 @@ public class Runner {
         System.out.println("version 1");
         System.out.println("version 2");
         System.out.println("version 3");
-        System.out.println("version 4");
 
 
     }
